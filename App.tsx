7e13d4a251cfa3375059fc61f3fd8f874b1e3312
valeria.luna <valeria.luna-ext@anbima.com.br>
2024-05-31 11:00:52 -0300

    feat: adiciona a StatusBar

--- App.tsx
+++ App.tsx
@@ -1,6 +1,16 @@
 import React from "react";
+import { StatusBar } from "react-native";
 import Home from "./src/screens/Home/Home";
 
 export default function App() {
-  return <Home />;
+  return (
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Home />
+    </>
+  );
 }
